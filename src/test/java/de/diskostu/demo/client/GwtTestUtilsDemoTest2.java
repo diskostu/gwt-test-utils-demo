a9c52d1b15a346b8e97bfd2d2decdccd2f698e77
diskostu <denny.kluge@gmail.com>
2018-02-18 15:38:40 +0100

    another test

--- src/test/java/de/diskostu/demo/client/GwtTestUtilsDemoTest2.java
+++ src/test/java/de/diskostu/demo/client/GwtTestUtilsDemoTest2.java
@@ -1,7 +1,9 @@
 package de.diskostu.demo.client;
 
+import com.google.gwt.user.client.ui.Label;
 import com.googlecode.gwt.test.GwtModule;
 import com.googlecode.gwt.test.GwtTest;
+import com.googlecode.gwt.test.assertions.GwtAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,5 +13,11 @@ public class GwtTestUtilsDemoTest2 extends GwtTest {
     @Test
     public void testDemo1() {
         Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void testDemo2() {
+        Label lala = new Label("lala");
+        GwtAssertions.assertThat(lala).textEquals("lala");
     }
 }
