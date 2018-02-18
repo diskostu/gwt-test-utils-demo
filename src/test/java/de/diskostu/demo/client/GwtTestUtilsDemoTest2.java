9acbeb232c202911f8ceeb23f7fd2da02c96c6b0
diskostu <denny.kluge@gmail.com>
2018-02-18 22:07:17 +0100

    test refinement

--- src/test/java/de/diskostu/demo/client/GwtTestUtilsDemoTest2.java
+++ src/test/java/de/diskostu/demo/client/GwtTestUtilsDemoTest2.java
@@ -4,20 +4,15 @@ import com.google.gwt.user.client.ui.Label;
 import com.googlecode.gwt.test.GwtModule;
 import com.googlecode.gwt.test.GwtTest;
 import com.googlecode.gwt.test.assertions.GwtAssertions;
-import org.junit.Assert;
 import org.junit.Test;
 
 @GwtModule("de.diskostu.demo.GwtTestUtilsDemo")
 public class GwtTestUtilsDemoTest2 extends GwtTest {
 
     @Test
-    public void testDemo1() {
-        Assert.assertEquals(1, 1);
-    }
-
-    @Test
     public void testDemo2() {
-        Label lala = new Label("lala");
-        GwtAssertions.assertThat(lala).textEquals("lala");
+        final String labeltext = "DemoText";
+        final Label lala = new Label(labeltext);
+        GwtAssertions.assertThat(lala).textEquals(labeltext);
     }
 }
